e463cfeafa397105b46212ad4b7ec463f3aaf07f
titofrota <italoeduardo.df@hotmail.com>
2024-11-25 15:02:53 -0300

    feat(cart-items): update models

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,8 @@
 class CartItem < ApplicationRecord
+  belongs_to :cart
+  belongs_to :product
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :total_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
