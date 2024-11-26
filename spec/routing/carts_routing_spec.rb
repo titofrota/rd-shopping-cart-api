25b7257a947866501b06b4d31b273e5f458b67a7
titofrota <italoeduardo.df@hotmail.com>
2024-11-25 23:05:29 -0300

    test: carts controller routing

--- spec/routing/carts_routing_spec.rb
+++ spec/routing/carts_routing_spec.rb
@@ -6,12 +6,16 @@ RSpec.describe CartsController, type: :routing do
       expect(get: '/cart').to route_to('carts#show')
     end
 
-    it 'routes to #create' do
-      pending "#TODO: Escreva um teste para validar a criação de um carrinho #{__FILE__}" 
+    it 'routes to #add_product' do
+      expect(post: '/cart').to route_to('carts#add_product')
     end
 
-    it 'routes to #add_item via POST' do
+    it 'routes to #add_item' do
       expect(post: '/cart/add_item').to route_to('carts#add_item')
     end
+
+    it 'routes to #remove_item' do
+      expect(delete: '/cart/:product_id').to route_to('carts#remove_item', product_id: ':product_id')
+    end
   end
-end 
+end
